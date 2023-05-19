0737761b777377f457c6b05b209a98cd312770e5
Ashley Thomas <115642737+ashleyth0mas@users.noreply.github.com>
2023-05-19 15:22:44 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //syntax to call controller
+  
     Get.find<Popularproductcontroller>().getproductlistcontroller();
      Get.find<Recommendedproductcontroller>().getRecommendedproductlist();
     return GetMaterialApp(
